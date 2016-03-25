4a1137931810b1566799516918e0842b89cddb2d
ming <ming.rutar@frontier.com>
2016-03-25 03:10:32 -0700

    add and delete works

--- myjpa/src/main/java/com/example/InventoryRepository.java
+++ myjpa/src/main/java/com/example/InventoryRepository.java
@@ -1,11 +1,13 @@
 package com.example;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface InventoryRepository extends CrudRepository<Item, Long> {
 
-	@RestResource(path="find")
-	String findByLabelIgnoreCase(@Param("label") String label);
+//	@RestResource(path="find")
+	Optional<Item> findByLabelIgnoreCase(@Param("label") String label);
 }
