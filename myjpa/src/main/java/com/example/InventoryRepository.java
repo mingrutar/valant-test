4fad9eb1960ec066454b201e1d38294d17778813
ming <ming.rutar@frontier.com>
2016-03-25 02:12:52 -0700

    add restresource

--- myjpa/src/main/java/com/example/InventoryRepository.java
+++ myjpa/src/main/java/com/example/InventoryRepository.java
@@ -2,7 +2,10 @@ package com.example;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface InventoryRepository extends CrudRepository<Item, Long> {
+
+	@RestResource(path="find")
 	String findByLabelIgnoreCase(@Param("label") String label);
 }
